6a0cdbc7b733b59697f6c2563264218b977321bb
Soffira <sofiacher7@ya.ru>
2018-10-02 18:57:41 +0500

    Удаленные файлы

--- lesson_1/geekshop/main/urls.py
+++
@@ -1,7 +0,0 @@
-#urls для main
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main_index),
-]
--- lesson_1/geekshop/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-#from django.http import HttpResponse, JsonResponse
-
-#возвращает страницу со списком contact
-
-def main_index(request):
-
-    return render(request, 'main/index.html', {})
