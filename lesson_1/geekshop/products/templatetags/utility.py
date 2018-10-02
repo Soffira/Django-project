fa1752e565d2118602b4cc1d330706be7e2d3af2
Soffira <sofiacher7@ya.ru>
2018-10-02 15:11:25 +0500

    Create folder templatetags

---
+++ lesson_1/geekshop/products/templatetags/utility.py
@@ -0,0 +1,11 @@
+from django import template
+
+register = template.Librery()
+
+@register.simple_tag  #декоратор превращает функцию в соответствующий template tag
+def get_value_by_key(source, key):
+    return source.get(key)
+    
+@register.filter(name='even')
+def get_even_items(items):
+    return (itm for idx, itm in enumerate(items, 1) if idx % 2 == 0) #фильтр, позволяет вывести на экран только четные элементы
