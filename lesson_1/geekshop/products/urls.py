2e0ce27b73be5c35ab69715a6f5ac19334142683
Soffira <sofiacher7@ya.ru>
2018-10-02 15:22:45 +0500

    Added urls for products

--- lesson_1/geekshop/products/urls.py
+++ lesson_1/geekshop/products/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
-    path('products/', views.products_catalog),
-    path('detail/', views.products_detail),
-    path('create/', views.products_create),
+    path('<int:pk>/', views.product_detail, name='detail'),
+    path('get_template/', views.get_catalog_template),
+    path('render_to_string/', views.render_catalog_to_string),
+    path('native/', views.native),
+    path('', views.product_catalog, name='catalog'),
 ]
