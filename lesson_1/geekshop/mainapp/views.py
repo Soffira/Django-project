7e8db20ff4e1915da8e1d1da89c187293e174b48
Soffira <sofiacher7@ya.ru>
2018-10-08 16:39:48 +0500

    Просмотр категорий с главной страницы

--- lesson_1/geekshop/mainapp/views.py
+++ lesson_1/geekshop/mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 #from django.http import HttpResponse, JsonResponse
 from datetime import datetime
+from django.shortcuts import get_object_or_404
 
 #возвращает страницу со списком list
 def main(request):
@@ -9,13 +10,33 @@ def main(request):
     # content = {'title': title, 'products': products}
     return render(request, 'mainapp/index.html', {})
 
-def products(request):
-    # title = 'камни'
-    # links_menu = ProductCategory.objects.all()
-    # same_products = Product.objects.all()
-    
-    # content = {'title': title, 'links_menu': links_menu, 'same_products': same_products}
-    return render(request, 'mainapp/catalog.html', {})
+def products(request, pk=None):
+    print(pk)
+
+    title = 'камни'
+    links_menu = Category.objects.all()
+     
+    if pk:
+        if pk == '0':
+            category = {'name': 'все'}
+            products = Product.objects.all().order_by('price')
+        else:
+            category = get_object_or_404(Category, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+        }
+        return render(request, 'products/products_list.html', content)
+    same_products = Product.objects.all()[3:5]
+    content = {
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
+    }
+    return render(request, 'products/products.html', content)
 
 def contact(request):
     # title = 'о нас'
